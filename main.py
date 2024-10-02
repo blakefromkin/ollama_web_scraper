acd580fb301ddaef26677f2ee1ec364cabf1871c
Blake Fromkin <blakefromkin@gmail.com>
2024-10-02 09:26:40 -0400

    feat: Display cleaned DOM content on landing page with a streamlit expander and text area

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import streamlit as st
-from scrape import scrape_website
+from scrape import (
+    scrape_website,
+    extract_body_content,
+    clean_body_content,
+    split_dom_content
+)
 
 st.title("AI Web Scraper")
 url = st.text_input("Enter a website URL: ")
@@ -8,6 +13,12 @@ if st.button("Scrape Site"):
     if url:
         st.write("Scraping the website...")
         result = scrape_website(url)
-        print(result)
+        body_content = extract_body_content(result)
+        cleaned_content = clean_body_content(body_content)
+
+        st.session_state.dom_content = cleaned_content
+
+        with st.expander("View DOM content"):
+            st.text_area("DOM content", cleaned_content, height=300)
     else:
         st.write("Must enter valid URL.")
