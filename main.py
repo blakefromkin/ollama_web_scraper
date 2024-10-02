79e20d2e27b0fc863a1469f4b391a3676e25c68e
Blake Fromkin <blakefromkin@gmail.com>
2024-10-02 09:17:23 -0400

    fix: added conditional to account for empty URL entry

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ st.title("AI Web Scraper")
 url = st.text_input("Enter a website URL: ")
 
 if st.button("Scrape Site"):
-    st.write("Scraping the website...")
-    result = scrape_website(url)
-    print(result)
+    if url:
+        st.write("Scraping the website...")
+        result = scrape_website(url)
+        print(result)
+    else:
+        st.write("Must enter valid URL.")
